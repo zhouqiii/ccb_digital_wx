8a7ac54944b24005bb15f1085e473f6c0ba37253
zhouqi <860178804@qq.com>
2022-08-12 15:32:13 +0800

    补充知识点

--- pages/testic/test.js
+++ pages/testic/test.js
@@ -8,7 +8,7 @@ Page({
   data: {
     name: 'footer',
     goodsNumber:[1,2,3,4,5,6,7,8,9,11,13,14],
-    toTop: ''
+    toTop: '',
   },
   onClickLeft() {
     console.log(utils.getImageurl('iamgeaaddress.png'))
